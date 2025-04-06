e63d0bc6ec73c2376a48b17c586e8cc194eff3b0
pbh914 <52387364+ArkBB@users.noreply.github.com>
2025-04-06 18:07:25 +0900

    refactor : 예외 처리
    간단한 예외처리 및 null 처리 학습

--- src/main/java/cleancode/minesweeper/tobe/MinesweeperGame.java
+++ src/main/java/cleancode/minesweeper/tobe/MinesweeperGame.java
@@ -25,20 +25,28 @@ public class MinesweeperGame {
         initializeGame();
 
         while (true) {
-            showBoard();
+            try {
+                showBoard();
 
-            if (doesUserWinTheGame()) {
-                System.out.println("지뢰를 모두 찾았습니다. GAME CLEAR!");
-                break;
-            }
-            if (doesUserLoseTheGame()) {
-                System.out.println("지뢰를 밟았습니다. GAME OVER!");
-                break;
-            }
+                if (doesUserWinTheGame()) {
+                    System.out.println("지뢰를 모두 찾았습니다. GAME CLEAR!");
+                    break;
+                }
+                if (doesUserLoseTheGame()) {
+                    System.out.println("지뢰를 밟았습니다. GAME OVER!");
+                    break;
+                }
 
-            String cellInput = getCellInputFromUser();
-            String userActionInput = getActionInputFromUser();
-            actOnCell(cellInput, userActionInput);
+                String cellInput = getCellInputFromUser();
+                String userActionInput = getActionInputFromUser();
+                actOnCell(cellInput, userActionInput);
+            } catch (AppException e) {
+                System.out.println(e.getMessage());
+
+            } catch (Exception e) {
+                System.out.println("프로그램에 문제가 생겼습니다.");
+                e.printStackTrace();
+            }
         }
     }
 
@@ -121,11 +129,15 @@ public class MinesweeperGame {
     private static boolean isAllCellOpened() {
         return Arrays.stream(BOARD)
                 .flatMap(stringArr -> Arrays.stream(stringArr))
-                .noneMatch(cell -> cell.equals(CLOSED_CELL_SIGN));
+                .noneMatch(cell -> CLOSED_CELL_SIGN.equals(cell));
     }
 
     private static int convertRowFrom(char cellInputRow) {
         int selectedRowIndex = Character.getNumericValue(cellInputRow) - 1;
+        if(selectedRowIndex >= BOARD_ROW_SIZE){
+            throw new IllegalArgumentException("잘못된 ROW 입력입니다.");
+        }
+
         return selectedRowIndex;
     }
 
@@ -171,8 +183,7 @@ public class MinesweeperGame {
                 selectedColIndex = 9;
                 break;
             default:
-                selectedColIndex = -1;
-                break;
+                throw new IllegalArgumentException("잘못된 COL 입력입니다.");
         }
         return selectedColIndex;
     }
